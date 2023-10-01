37eaf598d00f6039c2795715d01a9f37f50e91d6
Christian <nortecc@gmx.net>
2023-10-01 21:24:36 +0200

    added parameter telegram:prefix

--- app/mqtt_watchdog.py
+++ app/mqtt_watchdog.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-# Version 1.1 14.07.2023 15:45 -> update
+# Version 1.2 01.10.2023
 #
 #
 # todos:
-# statusabfrage in Telegram
+# prüfung von min|max Grenzwerten
 
 import json
 import paho.mqtt.client as mqtt 
@@ -14,15 +14,18 @@ import requests
 import logging
 from threading import Thread
 
-VERSION = "1.1 14.07.2023 15:45"
+VERSION = "1.2 01.10.2023"
 
 # main dictionary
 # example:
-# list[1]["topic"] = "vzlogger/chn0/agg"
-# list[1]["name"] = "Stromzaehler Einspeisung"
-# list[1]["expiry"] = 300
-# list[1]["last"] = "1345654789"
-# list[1]["status"] = "OK"
+# list[1]["topic"] = "vzlogger/chn0/agg"        -> aus config
+# list[1]["name"] = "Stromzaehler Einspeisung"  -> aus config
+# list[1]["expiry"] = 300                       -> aus config
+# list[1]["min"] = 10                           -> aus config
+# list[1]["max"] = 80                           -> aus config
+# list[1]["value"] = 13.5                       -> aus on_message
+# list[1]["last"] = "1345654789"                -> aus on_message
+# list[1]["status"] = "OK"                      -> aus while-Schleife
 list = []
 
 logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d.%m.%Y %H:%M:%S')
@@ -35,6 +38,7 @@ BROKER_PORT =       aList['broker']['port']
 BROKER_TOPIC =      aList['broker']['topic']
 TELEGRAM_TOKEN =    aList['telegram']['TOKEN']
 TELEGRAM_CHAT_ID =  aList['telegram']['chat_id']
+TELEGRAM_PREFIX =   aList['telegram']['prefix']
 
 # prefill timestamp-field in dictionary 
 t = datetime.now()
@@ -49,7 +53,7 @@ client = mqtt.Client()
 ############################################################################ TELEGRAM
 # send telegram message
 def send_telegram(message):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={message}"
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={TELEGRAM_PREFIX} | {message}"
     requests.get(url).json()
 
 ############################################################################ MQTT
@@ -86,7 +90,9 @@ def on_message(client, userdata, msg):
     timestamp = int(round(t.timestamp()))
     for i in range(len(list)):
         if list[i]["topic"] == msg.topic :
-           list[i].update(last=timestamp)
+            list[i].update(last=timestamp)
+            #list[i].update(value=str(msg.payload))
+            #logging.info("New Update for: " + str(list[i]["topic"]) + " last: " + str(list[i]["last"]) + " value: " + str(list[i]["value"]))
 
 # if broker goes offline
 def on_disconnect(client, userdata, rc):
@@ -155,6 +161,6 @@ while True:
                     send_telegram("✅ Reset Status of topic " + list[i]["name"] + " from critical back to OK")
                 case _:
                     set_state(i,"warning")
-    
+
     publish_state()
     time.sleep(30)
